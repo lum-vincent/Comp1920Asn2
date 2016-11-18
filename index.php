a342a2a57995edc6074bd301f67f7aa64305fa04
Vincent Lum <lum.vincent@gmail.com>
2016-11-17 19:13:49 -0800

    Initializing project

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+
+echo '
+<p><a href="./login.php">Login</a></p>
+<p><a href="./create.php">Create</a></p>
+'
+
+?>
---
+++ login.php
@@ -0,0 +1,16 @@
+<?php
+
+
+if(true) {
+
+	echo '
+	<form action="login.php" method="POST">
+		<label>Email</label>
+		<input type="email" name="email" /><br />
+		<label>Password</label>
+		<input type="password" name="password" /><br />
+	</form>
+	'
+}
+
+?>
